d6c1aa4358afab4f37acaa5518500f46127ed8bc
danba340 <danba340@gmail.com>
2019-12-12 02:47:14 +0100

    update server

--- server.js
+++ server.js
@@ -12,9 +12,16 @@ const server = express()
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+io.on("connection", function(socket) {
+  socket.join(socket.handshake.query.sessionId);
+
+  socket.on("paused", function(msg) {
+    io.sockets.in(socket.handshake.query.sessionId).emit("paused", msg);
+  });
+
+  socket.on("playing", function(msg) {
+    io.sockets.in(socket.handshake.query.sessionId).emit("playing", msg);
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
